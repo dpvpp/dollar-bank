8fc489433412fb6d2eda3019ec0812269fb3e87e
dpvpp <dpvpp22@yahoo.com>
2021-04-06 12:32:50 -0400

    Account and Customer initial implementations

--- src/com/dollarsbank/model/Account.java
+++ src/com/dollarsbank/model/Account.java
@@ -1,5 +1,28 @@
 package com.dollarsbank.model;
 
 public class Account {
-
+	
+	private long balance;
+	
+	Account() {
+		this.balance = 0;
+	}
+	
+	Account(long initBalance) {
+		this.balance = initBalance;
+	}
+	
+	public long getBalance() {
+		return this.balance;
+	}
+	
+	public long withdraw(long amount) {
+		this.balance -= amount;
+		return amount;
+	}
+	
+	public void deposit(long amount) {
+		this.balance += amount;
+	}
+	
 }
--- src/com/dollarsbank/model/Customer.java
+++ src/com/dollarsbank/model/Customer.java
@@ -1,5 +1,60 @@
 package com.dollarsbank.model;
 
 public class Customer {
+	
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	private String userId;
+	private String password;
+	
+	public Customer(String firstName, String lastName, String phoneNumber, String userId, String password) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+		this.userId = userId;
+		this.password = password;
+	}
 
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
