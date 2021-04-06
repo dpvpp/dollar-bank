34dcde30ab7a4a089ec07bf4446e2489e638c217
dpvpp <dpvpp22@yahoo.com>
2021-04-06 12:10:25 -0400

    Pimped out welcome screen

--- src/com/dollarsbank/application/DollarsBankApplication.java
+++ src/com/dollarsbank/application/DollarsBankApplication.java
@@ -8,6 +8,6 @@ public class DollarsBankApplication {
 	public static void main(String[] args) {
 		
 		System.out.println(ColorsUtility.BLUE.COLOR + "I'm blue da ba dee dabba da-ee" + ColorsUtility.RESET.COLOR);
-		ConsolePrinterUtility.welcomeBanner();
+		ConsolePrinterUtility.welcomeScreen();
 	}
 }
--- src/com/dollarsbank/utility/ConsolePrinterUtility.java
+++ src/com/dollarsbank/utility/ConsolePrinterUtility.java
@@ -2,12 +2,17 @@ package com.dollarsbank.utility;
 
 public class ConsolePrinterUtility {
 	
-	public static void welcomeBanner() {
-		System.out.println(ColorsUtility.GREEN.COLOR + "+-----------------------------------------+");
-		System.out.println("|                                     " + ColorsUtility.YELLOW.COLOR  + "O" + ColorsUtility.GREEN.COLOR + "   |");
-		System.out.println("|      Welcome to the Dollars Bank   " + ColorsUtility.YELLOW.COLOR  + "OOO" + ColorsUtility.GREEN.COLOR + "  |");
-		System.out.println("|                                   " + ColorsUtility.YELLOW.COLOR  + "OOOOO" + ColorsUtility.GREEN.COLOR + " |");
-		System.out.println("+-----------------------------------------+" + ColorsUtility.RESET.COLOR);
+	public static void welcomeScreen() {
+		System.out.println(ColorsUtility.GREEN.COLOR + "+---------------------------------------------------+");
+		System.out.println("|                                               " + ColorsUtility.YELLOW.COLOR  + "O" + ColorsUtility.GREEN.COLOR + "   |");
+		System.out.println("|      Welcome to the Big Money Dollars Bank   " + ColorsUtility.YELLOW.COLOR  + "OOO" + ColorsUtility.GREEN.COLOR + "  |");
+		System.out.println("|                                             " + ColorsUtility.YELLOW.COLOR  + "OOOOO" + ColorsUtility.GREEN.COLOR + " |");
+		System.out.println("+---------------------------------------------------+");
+		System.out.println(ColorsUtility.RED.COLOR + "WARNING: Big Money ballers only" + ColorsUtility.RESET.COLOR);
+		System.out.println("1. Create new Account");
+		System.out.println("2. Login");
+		System.out.println("3. Exit");
+		System.out.println("Select and option by typing 1, 2, or 3");
 	}
 	
 }
