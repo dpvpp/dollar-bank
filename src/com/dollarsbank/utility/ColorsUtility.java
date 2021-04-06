4112441397e23780543d828804c013198ff582a9
dpvpp <dpvpp22@yahoo.com>
2021-04-06 11:34:25 -0400

    Colors Utility implemented

--- src/com/dollarsbank/utility/ColorsUtility.java
+++ src/com/dollarsbank/utility/ColorsUtility.java
@@ -1,5 +1,14 @@
 package com.dollarsbank.utility;
 
-public class ColorsUtility {
-
+public enum ColorsUtility {
+	
+	RESET("\u001B[0m") , BLACK("\u001B[30m"), RED("\u001B[31m"), GREEN("\u001B[32m"), 
+	YELLOW("\u001B[33m"), BLUE("\u001B[34m"), PURPLE("\u001B[35m"), CYAN("\u001B[36m"),
+	WHITE("\u001B[37m");
+	
+	public final String COLOR;
+	
+	ColorsUtility(String value) {
+		this.COLOR = value;
+	}
 }
