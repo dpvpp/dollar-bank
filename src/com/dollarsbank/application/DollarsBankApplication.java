573877c920caeebd94baf3c3b737288d6f12f86a
dpvpp <dpvpp22@yahoo.com>
2021-04-06 16:01:48 -0400

    Made a main menu

--- src/com/dollarsbank/application/DollarsBankApplication.java
+++ src/com/dollarsbank/application/DollarsBankApplication.java
@@ -1,5 +1,7 @@
 package com.dollarsbank.application;
 
+import java.util.Scanner;
+
 import com.dollarsbank.utility.ColorsUtility;
 import com.dollarsbank.utility.ConsolePrinterUtility;
 
@@ -8,6 +10,42 @@ public class DollarsBankApplication {
 	public static void main(String[] args) {
 		
 		System.out.println(ColorsUtility.BLUE.COLOR + "I'm blue da ba dee dabba da-ee" + ColorsUtility.RESET.COLOR);
-		ConsolePrinterUtility.welcomeScreen();
+		mainMenu();
 	}
+	
+	public static void mainMenu() {
+		Scanner in = new Scanner(System.in);
+		
+		ConsolePrinterUtility.welcomeScreen();
+		boolean exit = false;
+		while(!exit) {
+			
+			ConsolePrinterUtility.inputArrow();;
+			if(in.hasNextInt()) {
+				int input = in.nextInt();
+				switch(input) { 
+					case 1 : 
+						createAccount();
+						break;
+					case 3 :
+						ConsolePrinterUtility.exitScreen();
+						exit = true;
+						break;
+					default :
+						ConsolePrinterUtility.printInvalidOption();
+						break;
+				}
+			} else {
+				ConsolePrinterUtility.printInvalidOption();
+			}
+			in.nextLine();
+			
+		}
+		in.close();
+	}
+	
+	public static void createAccount() {
+		ConsolePrinterUtility.createAccountBanner();
+	}
+	
 }
--- src/com/dollarsbank/utility/ConsolePrinterUtility.java
+++ src/com/dollarsbank/utility/ConsolePrinterUtility.java
@@ -15,4 +15,29 @@ public class ConsolePrinterUtility {
 		System.out.println("Select an option by typing 1, 2, or 3");
 	}
 	
+	public static void printInvalidOption() {
+		System.out.println(ColorsUtility.RED.COLOR + "+---------------------------------------------------------------+");
+		System.out.println("| WARNING! The option you have selected is not valid, try again |");
+		System.out.println("+---------------------------------------------------------------+" + ColorsUtility.RESET.COLOR);
+	}
+	
+	public static void createAccountBanner() {
+		System.out.println(ColorsUtility.BLUE.COLOR + "+---------------------------------------------------+");
+		System.out.println("| Welcome! Enter the details for your account below |");
+		System.out.println("+---------------------------------------------------+" + ColorsUtility.RESET.COLOR);
+	}
+	
+	public static void exitScreen() {
+		System.out.println(ColorsUtility.GREEN.COLOR + "+--------------------------------------------------------+");
+		System.out.println("|                                                  " + ColorsUtility.YELLOW.COLOR  + "O" + ColorsUtility.GREEN.COLOR + "     |");
+		System.out.println("|    Goodbye, if you make any big money while     " + ColorsUtility.YELLOW.COLOR  + "OOO" + ColorsUtility.GREEN.COLOR + "    |");
+		System.out.println("|        you're gone please come back and        " + ColorsUtility.YELLOW.COLOR  + "OOOOO" + ColorsUtility.GREEN.COLOR + "   |");
+		System.out.println("|               store it here       -------->   " + ColorsUtility.YELLOW.COLOR  + "OOOOOOO" + ColorsUtility.GREEN.COLOR + "  |");
+		System.out.println("|                                              " + ColorsUtility.YELLOW.COLOR  + "OOOOOOOOO" + ColorsUtility.GREEN.COLOR + " |");
+		System.out.println("+--------------------------------------------------------+" + ColorsUtility.RESET.COLOR);
+	}
+	
+	public static void inputArrow() {
+		System.out.print("> ");
+	}
 }
