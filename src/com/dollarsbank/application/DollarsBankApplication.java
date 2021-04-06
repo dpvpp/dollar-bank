aa2b53ab49376b2a6c5793de2cacd50cea96d735
dpvpp <dpvpp22@yahoo.com>
2021-04-06 11:59:45 -0400

    Welcome banner created

--- src/com/dollarsbank/application/DollarsBankApplication.java
+++ src/com/dollarsbank/application/DollarsBankApplication.java
@@ -1,12 +1,13 @@
 package com.dollarsbank.application;
 
 import com.dollarsbank.utility.ColorsUtility;
+import com.dollarsbank.utility.ConsolePrinterUtility;
 
 public class DollarsBankApplication {
 	
 	public static void main(String[] args) {
 		
-		System.out.print(ColorsUtility.BLUE.COLOR + "I'm blue da ba dee dabba da-ee" + ColorsUtility.RESET.COLOR);
-		
+		System.out.println(ColorsUtility.BLUE.COLOR + "I'm blue da ba dee dabba da-ee" + ColorsUtility.RESET.COLOR);
+		ConsolePrinterUtility.welcomeBanner();
 	}
 }
--- src/com/dollarsbank/utility/ConsolePrinterUtility.java
+++ src/com/dollarsbank/utility/ConsolePrinterUtility.java
@@ -1,5 +1,13 @@
 package com.dollarsbank.utility;
 
 public class ConsolePrinterUtility {
-
+	
+	public static void welcomeBanner() {
+		System.out.println(ColorsUtility.GREEN.COLOR + "+-----------------------------------------+");
+		System.out.println("|                                     " + ColorsUtility.YELLOW.COLOR  + "O" + ColorsUtility.GREEN.COLOR + "   |");
+		System.out.println("|      Welcome to the Dollars Bank   " + ColorsUtility.YELLOW.COLOR  + "OOO" + ColorsUtility.GREEN.COLOR + "  |");
+		System.out.println("|                                   " + ColorsUtility.YELLOW.COLOR  + "OOOOO" + ColorsUtility.GREEN.COLOR + " |");
+		System.out.println("+-----------------------------------------+" + ColorsUtility.RESET.COLOR);
+	}
+	
 }
